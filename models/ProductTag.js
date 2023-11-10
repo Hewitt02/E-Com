d73116043b58efba4593ed2f7e74170b872711d1
Hewitt02 <Hewitt1203@gmail.com>
2023-11-10 13:44:54 -0500

    finishing models codes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-  },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      }
+    }
+
+},
   {
     sequelize,
     timestamps: false,
