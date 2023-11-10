25ae76ccb54d3a9e86fb5a09839bdb93a5510168
Hewitt02 <Hewitt1203@gmail.com>
2023-11-10 13:44:07 -0500

    adding category codes

--- models/index.js
+++ models/index.js
@@ -5,14 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: 'category_id'
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany( Tag, {
+  foreignKey: 'product_id',
+  through: ProductTag,
+})
 // Tags belongToMany Products (through ProductTag)
 
-module.exports = {
+ag.belongsToMany( Product, {
+  foreignKey: 'tag_id',
+  through: ProductTag,
+ }) 
+
+ module.exports = {
   Product,
   Category,
   Tag,
